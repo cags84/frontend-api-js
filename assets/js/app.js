53617aa49d845a5290fa9ad48e909a0f6ec91fd3
Carlos Guzmán <cags84@gmail.com>
2021-05-17 12:25:21 -0500

    Implemented loadtweets at home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,62 @@
 const URL_BASE = 'http://api.makeitreal.local:3000';
 
+function timeFromNow(time) {
+  // Credits to
+  // https://gomakethings.com/a-vanilla-js-alternative-to-the-moment.js-timefromnow-method/
+  // https://www.toptal.com/software/definitive-guide-to-datetime-manipulation
+  // Get timestamps
+  const unixTime = new Date(time).getTime();
+  if (!unixTime) return;
+  const now = new Date().getTime();
+
+  // Calculate difference
+  let difference = unixTime / 1000 - now / 1000;
+
+  // Setup return object
+  const tfn = {};
+
+  // Check if time is in the past, present, or future
+  tfn.when = 'now';
+  if (difference > 0) {
+    tfn.when = 'future';
+  } else if (difference < -1) {
+    tfn.when = 'past';
+  }
+
+  // Convert difference to absolute
+  difference = Math.abs(difference);
+
+  // Calculate time unit
+  if (difference / (60 * 60 * 24 * 365) > 1) {
+    // Years
+    tfn.unitOfTime = 'y';
+    tfn.time = Math.floor(difference / (60 * 60 * 24 * 365));
+  } else if (difference / (60 * 60 * 24 * 45) > 1) {
+    // Months
+    tfn.unitOfTime = 'm';
+    tfn.time = Math.floor(difference / (60 * 60 * 24 * 45));
+  } else if (difference / (60 * 60 * 24) > 1) {
+    // Days
+    tfn.unitOfTime = 'd';
+    tfn.time = Math.floor(difference / (60 * 60 * 24));
+  } else if (difference / (60 * 60) > 1) {
+    // Hours
+    tfn.unitOfTime = 'h';
+    tfn.time = Math.floor(difference / (60 * 60));
+  } else if (difference / 60 > 1) {
+    // Hours
+    tfn.unitOfTime = 'min';
+    tfn.time = Math.floor(difference / 60);
+  } else {
+    // Seconds
+    tfn.unitOfTime = 's';
+    tfn.time = Math.floor(difference);
+  }
+
+  // Return time from now data
+  return tfn;
+}
+
 function removeForm() {
   const divForm = document.querySelector('div.form');
   divForm.parentElement.removeChild(divForm);
@@ -90,6 +147,53 @@ function logout() {
   showFormLogin();
 }
 
+function loadTweets() {
+  const articles = document.getElementById('articles');
+  const options = {
+    method: 'GET',
+    withCredentials: true,
+    credentials: 'include',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  fetch(`${URL_BASE}/api/tweets/`, options)
+    .then((res) => res.json())
+    .then((json) => {
+      if (json.data.length > 0) {
+        articles.innerHTML = '';
+        const template = document.getElementById('template-tweets').content;
+
+        // eslint-disable-next-line no-plusplus
+        for (let i = 0; i < json.data.length; i++) {
+          const difference = timeFromNow(json.data[0].createdAt);
+
+          const spamUser = template.getElementById('tweets-user');
+          spamUser.innerHTML = `${json.data[i].user.name}`;
+
+          const spamUsername = template.getElementById('tweets-username');
+          spamUsername.innerHTML = `${json.data[i].user.username}`;
+
+          const textDate = template.getElementById('tweets-date');
+          textDate.innerHTML = `${difference.time}${difference.unitOfTime}`;
+
+          const textMessage = template.getElementById('tweet-message');
+          textMessage.innerHTML = `${json.data[i].content}`;
+
+          const fragment = document.createDocumentFragment();
+          const clone = template.cloneNode(true);
+
+          fragment.appendChild(clone);
+          articles.appendChild(fragment);
+        }
+      } else {
+        const divArticles = document.getElementById('articles');
+        divArticles.innerHTML = '<p>You currently have no tweets!</p>';
+      }
+    });
+}
+
 function createTweet() {
   const input = document.getElementById('form-tweet');
   const tweet = {
@@ -114,6 +218,7 @@ function createTweet() {
     .then((json) => {
       // eslint-disable-next-line no-undef
       Notiflix.Notify.Success('Tweet creado corrrectamente');
+      loadTweets();
     })
     .catch((err) => {
       // eslint-disable-next-line no-undef
@@ -142,6 +247,8 @@ function showDashboard(username) {
     e.preventDefault();
     createTweet();
   });
+
+  loadTweets();
 }
 
 function init() {
